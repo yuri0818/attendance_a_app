96e00bb6c449f87ab725a1da78a1e0828319e4e1
yuri0818 <yyyy.0818.0830@gmail.com>
2020-05-30 05:51:36 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,21 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  
+   # 永続的セッションを記憶します（Userモデルを参照）
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  
+  def forget(user)
+    user.forget # Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # セッションと@current_userを破棄します
   def log_out
     session.delete(:user_id)
