74a5022fa6872f4dc7cda0d433175862eb873a70
yuri0818 <yyyy.0818.0830@gmail.com>
2020-06-01 10:24:57 +0000

    top-page-cusutomc-olor

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,13 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+    def format_hour(time)
+      format("%02d", (time.hour))
+  end
+
+
+  def format_min(time)
+      format("%02d", (time.min / 15 * 15 ))
+  end
 end
